cd6b6e020d8a79fb1f70e1c38ae49434896e9203
gim-yoseb <gim-yoseb@172.19.87.159>
2022-11-10 09:51:52 +0900

    First Code

---
+++ Please_Movie/Please_Movie/src/Please_Movie/MainFrame.java
@@ -0,0 +1,10 @@
+package Please_Movie;
+
+public class MainFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("김요");
+	}
+}
---
+++ Please_Movie/Please_Movie/src/Please_Movie/ShowInfo.java
@@ -0,0 +1,5 @@
+package Please_Movie;
+
+public class ShowInfo {
+
+}
