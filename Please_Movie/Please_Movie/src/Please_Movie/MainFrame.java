2aa473f38e41f2a557bda03606abb5ab4341d4a6
pas49 <pas49@DESKTOP-72DOJMI>
2022-11-17 09:44:59 +0900

    테스트용2

--- Please_Movie/Please_Movie/src/Please_Movie/MainFrame.java
+++ Please_Movie/Please_Movie/src/Please_Movie/MainFrame.java
@@ -5,7 +5,7 @@ public class MainFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("源��슂ㄴㅇㅁㄴsdㅇㄴㅁㅇㅁㄴ");
+		System.out.println("源��슂ㄴㅇㅁㄴsdㅇㄴㅁsadasdasdㅇㅁㄴ");
 		System.out.println("test");
 	}
 }
