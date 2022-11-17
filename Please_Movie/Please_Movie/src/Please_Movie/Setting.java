58b71651b636213f2b21af775822e7c5e61a7293
Jaemin Eun <tomy000120@naver.com>
2022-11-17 09:42:05 +0900

    hiyo

--- Please_Movie/Please_Movie/src/Please_Movie/Setting.java
+++ Please_Movie/Please_Movie/src/Please_Movie/Setting.java
@@ -1,5 +1,23 @@
 package Please_Movie;
 
 public class Setting {
-
+	private String appinfo;
+	private String userLocation = "코리아텍";
+	private int curTime = 1200;
+	public Setting(int input) {
+		this.curTime = input;
+	}
+	public void setUserLocation(String input) {
+		this.userLocation = input;
+	}
+	public String getUserLocation() {
+		return
+	}
+	public String setTime() {
+	
+	}
+	public String getTime() {
+		
+	}
+	public void displaySetting
 }
